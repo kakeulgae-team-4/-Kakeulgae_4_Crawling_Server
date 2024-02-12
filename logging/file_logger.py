5ac49c23ec38702ea5fc68a846aec1913203f595
koosco <rnxogud123@naver.com>
2024-02-12 13:01:29 +0900

    feat : file logger 추가

---
+++ logging/file_logger.py
@@ -0,0 +1,44 @@
+import logging
+
+
+class FileLogger:
+    logger = logging.getLogger(name='stream_logger')
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('|%(asctime)s||%(name)s||%(levelname)s||%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+    @staticmethod
+    def setLevel(level: str):
+        if level == "CRITICAL":
+            FileLogger.logger.setLevel(logging.CRITICAL)
+        if level == "FATAL":
+            FileLogger.logger.setLevel(logging.FATAL)
+        if level == "ERROR":
+            FileLogger.logger.setLevel(logging.ERROR)
+        if level == "WARNING":
+            FileLogger.logger.setLevel(logging.WARNING)
+        if level == "WARN":
+            FileLogger.logger.setLevel(logging.WARN)
+        if level == "INFO":
+            FileLogger.logger.setLevel(logging.INFO)
+        if level == "DEBUG":
+            FileLogger.logger.setLevel(logging.DEBUG)
+        if level == "NOTSET":
+            FileLogger.logger.setLevel(logging.NOTSET)
+
+    @staticmethod
+    def setPath(filePath: str='incruit/log.txt'):
+        file_handler = logging.FileHandler(filePath, mode='a', encoding='utf')
+        file_handler.setFormatter(FileLogger.formatter)
+        FileLogger.logger.addHandler(file_handler)
+
+
+    @staticmethod
+    def log(message: str):
+        FileLogger.logger.info(message)
+
+
+if __name__ == '__main__':
+    logger = FileLogger()
+    logger.setPath()
+    logger.log('test log!')
