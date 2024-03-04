5d6ee4f5548a91340f9166716ed972fa8bb7a183
koosco <rnxogud123@naver.com>
2024-03-04 21:50:51 +0900

    local db repository 추가

---
+++ scraping/repository/local_db.py
@@ -0,0 +1,109 @@
+from typing import Any
+
+from models import Career, WorkType, Education, Job, JobPostingCareer, JobPostingWorkType, RegionPostingRelation, \
+    Region2nd
+from repository import Repository
+
+
+class CareerRepository(Repository):
+
+    def save(self, item: Any):
+        pass
+
+    def find_one(self, id: int) -> Career:
+        return Career.objects.filter(id=id).first()
+
+    def find_by_type(self, type: str) -> Career:
+        return Career.objects.filter(type=type).first()
+
+
+class EducationRepository(Repository):
+    def save(self, item: Any):
+        pass
+
+    def find_one(self, id: int):
+        pass
+
+    def find_by_type(self, type: str) -> Education:
+        return Education.objects.filter(type=type).first()
+
+
+class JobRepository(Repository):
+    def save(self, item: Any):
+        pass
+
+    def find_one(self, id: int):
+        pass
+
+    def find_by_type(self, type: str) -> Job:
+        return Job.objects.filter(type=type).first()
+
+
+class JobPostingCareerRepository(Repository):
+    def save(self, item: JobPostingCareer) -> int:
+        item.save()
+        return item.id
+
+    def find_one(self, id: int) -> JobPostingCareer:
+        return JobPostingCareer.objects.filter(id=id).first()
+
+
+class JobPostingWorkTypeRepository(Repository):
+    def save(self, item: JobPostingWorkType) -> int:
+        item.save()
+        return item.id
+
+    def find_one(self, id: int) -> JobPostingWorkType:
+        return JobPostingWorkType.objects.filter(id=id).first()
+
+
+from repository import Repository
+from posting.models import JobPosting
+
+
+class PostRepository(Repository):
+    def save(self, item: JobPosting):
+        item.save()
+        return item.id
+
+    def find_one(self, post_id: int):
+        return JobPosting.objects.filter(id=post_id).first()
+
+
+class RegionPostingRelationRepository(Repository):
+    def save(self, item: RegionPostingRelation) -> int:
+        item.save()
+        return item.id
+
+    def find_one(self, id: int) -> RegionPostingRelation:
+        return RegionPostingRelation.objects.filter(id=id).first()
+
+
+class WorkTypeRepository(Repository):
+    def save(self, item: Any):
+        pass
+
+    def find_one(self, id: int) -> WorkType:
+        return WorkType.objects.filter(id=id).first()
+
+    def find_by_type(self, type: str) -> WorkType:
+        return WorkType.objects.filter(type=type).first()
+
+
+class Region2ndRepository(Repository):
+    def save(self, item: Any):
+        pass
+
+    def find_one(self, id: int):
+        pass
+
+    def find_by_type(self, type: str) -> Region2nd:
+        return Region2nd.objects.filter(type=type).first()
+
+
+class Region2ndPostingRepository(Repository):
+    def save(self, item: RegionPostingRelation):
+        item.save()
+
+    def find_one(self, id: int):
+        RegionPostingRelation.objects.filter(id=id).first()
--- scraping/service/post_service.py
+++ scraping/service/post_service.py
@@ -1,28 +1,42 @@
-import import_django
-from domain.post.post import Post
-from posting.models import JobPosting, Job, Education, Career
-from scraping.repository.post.local_post_repository import LocalPostRepository
-from scraping.repository.job_posting_career.local_job_posting_repository import LocalJobPostingCareerRepository
-from scraping.repository.job_posting_work_type.local_job_posting_work_type_repository import LocalJobPostingWorkTypeRepository
-from scraping.repository.region_posting_relation.local_region_posting_relation_repository import LocalRegionPostingRelationRepository
+from local_db import *
 
 
 class PostService:
-    post_repository = LocalPostRepository()
-    job_posting_career_repository = LocalJobPostingCareerRepository()
+    def __init__(self):
+        self.job_repository = JobRepository()
+        self.education_repository = EducationRepository()
+        self.post_repository = PostRepository()
+        self.job_posting_career_repository = JobPostingCareerRepository()
+        self.job_posting_work_type_repository = JobPostingWorkTypeRepository()
+        self.local_region_posting_repository = RegionPostingRelationRepository()
+        self.career_repository = CareerRepository()
+        self.work_type_repository = WorkTypeRepository()
+        self.region_2nd_repository = Region2ndRepository()
 
     def save_post(self, **kwargs):
+        job = self.job_repository.find_by_type(kwargs['job'])
+        education = self.education_repository.find_by_type(kwargs['education'])
+
         post = kwargs['post']
-        job = Job.objects.filter(type=kwargs['job']).first()
-        education = Education.objects.filter(type=kwargs['education']).first()
-        career = Career.objects.filter(type=kwargs['career']).first()
+        new_job_posting = JobPosting(company_name=post.company_name,
+                                     post_name=post.post_name,
+                                     job=job,
+                                     education=education,
+                                     url=post.url,
+                                     deadline=post.deadline,
+                                     created_at=post.created_at)
 
-        job_post = JobPosting(company_name=post.company_name,
-                              post_name=post.post_name,
-                              job=job,
-                              education=education,
-                              url=post.url,
-                              deadline=post.deadline,
-                              created_at=post.created_at)
+        self.post_repository.save(new_job_posting)
 
-        self.post_repository.save(job_post)
+        career = self.career_repository.find_by_type(kwargs['career'])
+        new_job_posting_career = JobPostingCareer(career=career,
+                                                  job_posting=new_job_posting)
+        self.job_posting_career_repository.save(new_job_posting_career)
+
+        new_work_type = self.work_type_repository.find_by_type(kwargs['work_type'])
+        new_job_posting_work_type = JobPostingWorkType(work_type=new_work_type,
+                                                       job_posting=new_job_posting)
+        self.job_posting_work_type_repository.save(new_job_posting_work_type)
+
+        region = self.region_2nd_repository.find_by_type('region_2nd')
+        self.region_2nd_repository.save(region)
