9d7d7cc4c3587ca3d201a7d8fd1bb755cabf451c
koosco <rnxogud123@naver.com>
2024-03-04 11:32:58 +0900

    django models 추가

--- posting/models.py
+++ posting/models.py
@@ -1,114 +1,123 @@
 from django.db import models
 
 
-class Bookmark(db.Model):
-    __tablename__ = 'bookmark'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    job_posting_id = db.Column(db.Integer, db.ForeignKey('job_posting.id', name='fk_bookmark_job_posting_id'))
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id', name='fk_bookmark_member_id'))
+class Education(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=90)
+
+    class Meta:
+        managed = False
+        db_table = 'education'
 
 
-class Career(db.Model):
-    __tablename__ = 'career'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(90))
-    post = db.relationship('Post', backref='career', lazy=True)
+class Job(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=25)
 
-    __table_args__ = (db.UniqueConstraint('type', name='career_uk'),
-                      db.Index('career_idx_type', 'type'))
-
-
-class Education(db.Model):
-    __tablename__ = 'education'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(90))
-    post = db.relationship('Post', backref='education', lazy=True)
-
-    __table_args__ = (db.UniqueConstraint('type', name='education_uk'),
-                      db.Index('education_idx_type', 'type'))
+    class Meta:
+        managed = False
+        db_table = 'job'
 
 
-class Job(db.Model):
-    __tablename__ = 'job'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(25))
-    job_detail = db.relationship('JobDetail', backref='job', lazy=True)
+class JobPosting(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    company_name = models.CharField(max_length=90, null=False, blank=False)
+    post_name = models.CharField(max_length=255, null=False, blank=False)
 
-    __table_args__ = (db.UniqueConstraint('type', name='job_uk'),
-                      db.Index('job_idx_type', 'type'))
+    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
+    education = models.ForeignKey(Education, on_delete=models.SET_NULL, null=True)
 
+    url = models.CharField(max_length=255, null=False, blank=False)
+    deadline = models.DateField(null=False, blank=False)
+    created_at = models.DateField(null=False, blank=False)
 
-class JobCategory(db.Model):
-    __tablename__ = 'job_category'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    type = db.Column(db.String(90))
-    job_detail = db.relationship('JobDetail', backref='job_category', lazy=True)
-
-    __table_args = (db.UniqueConstraint('type', name='job_category_uk'),
-                    db.Index('job_idx_type', 'type'))
+    class Meta:
+        managed = False
+        db_table = 'job_posting'
 
 
-class JobDetail(db.Model):
-    __tablename__ = 'job_detail'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(255))
-    job_id = db.Column(db.Integer, db.ForeignKey('job.id', name='job_fk'))
-    job_category_id = db.Column(db.Integer, db.ForeignKey('job_category.id', name='job_category_fk'))
-    post = db.relationship('Post', backref='job_detail', lazy=True)
+class Career(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=90)
 
-    __table_args__ = (db.UniqueConstraint('type', 'job_id', name='job_detail_uk'),
-                      db.Index('job_detail_idx_type', 'type'))
+    class Meta:
+        managed = False
+        db_table = 'career'
 
 
-class JobPost(db.Model):
-    __tablename__ = 'job_posting'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    # 회사명
-    company_name = db.Column(db.String(90), nullable=False)
-    # 공고명
-    post_name = db.Column(db.String(255), nullable=False)
-    # 경력명
-    career_id = db.Column(db.Integer, db.ForeignKey('career.id'))
-    # 교육수준명
-    education_id = db.Column(db.Integer, db.ForeignKey('education.id'))
-    # 채용분야명
-    job_detail_id = db.Column(db.Integer, db.ForeignKey('job_detail.id'))
-    # 지역명
-    region_2nd_id = db.Column(db.Integer, db.ForeignKey('region_2nd.id'))
-    # 채용형태명
-    work_type_id = db.Column(db.Integer, db.ForeignKey('work_type.id'))
-    deadline = db.Column(db.DateTime(), nullable=False)
-    url = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime(), nullable=False)
+class JobPostingCareer(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    career = models.ForeignKey(Career, on_delete=models.CASCADE)
+    job_posting = models.ForeignKey(JobPosting, on_delete=models.CASCADE)
 
-    def __repr__(self):
-        return (f'<Post {self.company_name}'
-                f', {self.post_name}'
-                f', {self.career}'
-                f', {self.education}'
-                f', {self.location}'
-                f', {self.job_type}'
-                f', {self.deadline}'
-                f', {self.url}'
-                f', {self.created_date}>')
-
-class Region1st(db.Model):
-    __tablename__ = 'region_1st'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(25))
-    region2nd = db.relationship('Region2nd', backref='region1st', lazy=True)
+    class Meta:
+        managed = False
+        db_table = 'job_posting_career'
 
 
-class Region2nd(db.Model):
-    __tablename__ = 'region_2nd'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(25))
-    region1st_id = db.Column(db.Integer, db.ForeignKey('region_1st.id'), nullable=False)
-    post = db.relationship('Post', backref='region_2nd', lazy=True)
+class WorkType(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=90)
+
+    class Meta:
+        managed = False
+        db_table = 'work_type'
 
 
-class WorkType(db.Model):
-    __tablename__ = 'work_type'
-    id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(25))
-    post = db.relationship('Post', backref='work_type', lazy=True)
+class JobPostingWorkType(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    job_posting = models.ForeignKey(JobPosting, on_delete=models.CASCADE, related_name='work_types')
+    work_type = models.ForeignKey(WorkType, on_delete=models.CASCADE, related_name='work_types')
+
+    class Meta:
+        managed = False
+        db_table = 'job_posting_work_type'
+
+
+class JobCategory(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=90)
+
+    class Meta:
+        managed = False
+        db_table = 'job_category'
+
+
+class JobDetail(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=90)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='job_details')
+    job_category = models.ForeignKey(JobCategory, on_delete=models.CASCADE, related_name='job_details')
+
+    class Meta:
+        managed = False
+        db_table = 'job_detail'
+
+
+class Region1st(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=25, null=False, blank=False)
+
+    class Meta:
+        managed = False
+        db_table = 'region_1st'
+
+
+class Region2nd(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    type = models.CharField(max_length=25)
+    region_1st = models.ForeignKey(Region1st, on_delete=models.SET_NULL)
+
+    class Meta:
+        managed = False
+        db_table = 'region_2nd'
+
+
+class RegionPostingRelation(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='region_posting_relations')
+    region_2nd = models.ForeignKey(Region2nd, on_delete=models.CASCADE, related_name='region_posting_relations')
+
+    class Meta:
+        managed = False
+        db_table = 'region_posting_relation'
