8ddf0c095d070c8c5687278b559f503d1d081808
koosco <rnxogud123@naver.com>
2024-03-24 12:44:27 +0900

    repository 삭제 -> service에서 repository 바로 호출하도록 변경

--- scraping/service/post_service.py
+++ scraping/service/post_service.py
@@ -1,44 +1,47 @@
 import import_django
-from scraping.repository.domain_repository import *
+from post.after_process_dto import AfterProcessDto
+from post.after_dto_builder import AfterDtoBuilder
 from posting_service.models import *
 
 
 class PostService:
-    def __init__(self):
-        self.job_repository = JobRepository()
-        self.education_repository = EducationRepository()
-        self.post_repository = PostRepository()
-        self.job_posting_career_repository = JobPostingCareerRepository()
-        self.job_posting_work_type_repository = JobPostingWorkTypeRepository()
-        self.local_region_posting_repository = RegionPostingRelationRepository()
-        self.career_repository = CareerRepository()
-        self.work_type_repository = WorkTypeRepository()
-        self.region_2nd_repository = Region2ndRepository()
+    def save_post(self, dto: AfterProcessDto):
+        job_details = []
+        for job_detail in dto.job_detail:
+            job_details.append(JobDetail.objects.get(type=job_detail))
+        education = Education.objects.get(type=dto.education)
+        career = Career.objects.get(type=dto.career)
+        work_type = WorkType.objects.get(type=dto.work_type)
+        region_2nd = Region2nd.objects.get(type=dto.region_2nd)
 
-    def save_post(self, **kwargs):
-        job = self.job_repository.find_by_type(kwargs['job'])
-        education = self.education_repository.find_by_type(kwargs['education'])
+        job_posting = JobPosting.objects.create(company_name=dto.company_name,
+                                                post_name=dto.post_name,
+                                                education=education,
+                                                url=dto.url,
+                                                deadline=dto.deadline,
+                                                created_at=dto.created_at)
+        RegionPostingRelation.objects.create(job_posting=job_posting, region_2nd=region_2nd)
+        JobPostingWorkType.objects.create(job_posting=job_posting, work_type=work_type)
+        JobPostingCareer.objects.create(job_posting=job_posting, career=career)
+        for job_detail in job_details:
+            JobDetailPostingRelation.objects.create(job_posting=job_posting, job_detail=job_detail)
 
-        post = kwargs['post']
-        new_job_posting = JobPosting(company_name=post.company_name,
-                                     post_name=post.post_name,
-                                     job=job,
-                                     education=education,
-                                     url=post.url,
-                                     deadline=post.deadline,
-                                     created_at=post.created_at)
 
-        self.post_repository.save(new_job_posting)
-
-        career = self.career_repository.find_by_type(kwargs['career'])
-        new_job_posting_career = JobPostingCareer(career=career,
-                                                  job_posting=new_job_posting)
-        self.job_posting_career_repository.save(new_job_posting_career)
-
-        new_work_type = self.work_type_repository.find_by_type(kwargs['work_type'])
-        new_job_posting_work_type = JobPostingWorkType(work_type=new_work_type,
-                                                       job_posting=new_job_posting)
-        self.job_posting_work_type_repository.save(new_job_posting_work_type)
-
-        region = self.region_2nd_repository.find_by_type('region_2nd')
-        self.region_2nd_repository.save(region)
+if __name__ == '__main__':
+    from datetime import datetime, timedelta
+    ps = PostService()
+    data = (AfterDtoBuilder()
+            .company_name("kakao corp")
+            .post_name("engineering")
+            .education("대졸")
+            .url("https://www.kakao.com")
+            .deadline((datetime.now() + timedelta(days=3)).strftime("%Y-%m-%d"))
+            .created_at((datetime.now() - timedelta(days=3)).strftime("%Y-%m-%d"))
+            .work_type("계약직")
+            .career("신입")
+            .job_detail(["웹개발", "ASP"])
+            .region_2nd("강남구")
+            .build()
+            )
+    print(data)
+    ps.save_post(data)
